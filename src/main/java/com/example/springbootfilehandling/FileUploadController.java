4581cbd9812be3d897f6ab9c24de8fa5da612a3e
AioTechz <yutthagarn.ken@gmail.com>
2018-09-28 17:47:37 +0700

    first commit

---
+++ src/main/java/com/example/springbootfilehandling/FileUploadController.java
@@ -0,0 +1,33 @@
+package com.example.springbootfilehandling;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cyl3erPunKz
+ * Date: 28 September 2018
+ * Time: 5:32 PM
+ **/
+@RestController
+public class FileUploadController {
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("/var/tmp/" + file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return "File is upload successfully";
+    }
+
+}
---
+++ src/test/java/com/example/springbootfilehandling/SpringBootFileHandlingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootfilehandling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFileHandlingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
